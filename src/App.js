5d8faeae3c6a565c44eaeea9f857c09dd83dc037
avinashpatilcoder <133511337+avinashpatilcoder@users.noreply.github.com>
2023-10-18 13:37:59 +0530

    Filter Functionality Added
    Filter Functionality Added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Popup from './components/Popup';
 function App() {
 
   const initialData = [
-    { id: 'ezest1', name: 'John', sno: 1, designation: 'HR', description: 'HR Manager', doj: '01/01/2023'},
+    { id: 'ezest1', name: 'Sagar', sno: 1, designation: 'HR', description: 'HR Manager', doj: '01/01/2023'},
     { id: 'ezest2', name: 'Avi', sno: 2, designation: 'Full Stack', description: 'Developer', doj: '01/01/2023' },
     { id: 'ezest3', name: 'Akshay', sno: 3, designation: 'UI/UX', description: 'Developer', doj: '01/01/2023' },
     { id: 'ezest4', name: 'Aditi', sno: 4, designation: 'React', description: 'Developer', doj: '01/01/2023' },
@@ -19,6 +19,15 @@ function App() {
   const [errorMessages, setErrorMessages] = useState({});
   const [newRow, setnewRow] = useState(false);
   const [oldRow, setoldRow] = useState(false);
+  const [showName, setshowName] = useState(true)
+  const [showSrno, setshowSrno] = useState(true)
+  const [showEpid, setshowEpid] = useState(true)
+  const [showDescription, setshowDescription] = useState(true)
+  const [showDesignation, setshowDesignation] = useState(true)
+  const [showDoj, setshowDoj] = useState(true)
+  const [showEdit, setshowEdit] = useState(true)
+  const [showColumn, setshowColumn] = useState(false)
+
 
   const openPopup = () => {
     setIsPopupOpen(true);
@@ -34,15 +43,47 @@ function App() {
     setErrorMessages({});
   };
 
+  const handleColumn = () => {
+    setshowColumn(!showColumn)
+  }
+
   const handleEdit = (row) => {
     setSelectedRow(row);
     setoldRow(true);
     openPopup();
   };
 
+  const handleSrno = () => {
+    setshowSrno(!showSrno);
+  }
+
+  const handleName = () => {
+    setshowName(!showName)
+  }
+
+  const handleEmip = () => {
+    setshowEpid(!showEpid)
+  }
+
+  const handleDesignation = () => {
+    setshowDesignation(!showDesignation)
+  }
+
+  const handleDescription = () => {
+    setshowDescription(!showDescription);
+  }
+
+  const handleDoj = () => {
+    setshowDoj(!showDoj)
+  }
+
+  const handleShowEdit = () => {
+    setshowEdit(!showEdit);
+  }
+
   const handleSave = (updatedRow) => {
     if (!updatedRow.name || !updatedRow.doj || !updatedRow.designation) {
-      setErrorMessages({ requiredFields: 'All fields are mandatory.' });
+      setErrorMessages({ requiredFields: 'Except Description All fields are mandatory.' });
       return;
     }
 
@@ -76,7 +117,30 @@ function App() {
     <div>
       <h1>Dynamic Table</h1>
       <button onClick={handleNew}>Add New Row</button>
-      <Table data={data} onEdit={handleEdit} />
+      <button onClick={handleColumn}>Column Filter</button>
+      {
+        showColumn ? <div>
+          <button onClick={handleSrno}>SrNo</button>
+          <button onClick={handleName}>Name</button>
+          <button onClick={handleEmip}>Empid</button>
+          <button onClick={handleDesignation}>Designation</button>
+          <button onClick={handleDescription}>Description</button>
+          <button onClick={handleDoj}>Date of Joining</button>
+          <button onClick={handleShowEdit}>Edit</button>
+        </div>: ''
+      }
+      <Table data={data} onEdit={handleEdit} 
+      showSrno={showSrno}
+      showName={showName}
+      showEpid={showEpid}
+      showDoj={showDoj}
+      showDesignation={showDesignation}
+      showDescription={showDescription}
+      showEdit={showEdit}
+      
+      
+      
+      />
       {isPopupOpen && (
         <Popup
           data={selectedRow}
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,31 +1,45 @@
-import React from 'react';
+import React from "react";
 
-function Table({ data, onEdit }) {
+function Table({
+  data,
+  onEdit,
+  showSrno,
+  showName,
+  showDescription,
+  showDesignation,
+  showDoj,
+  showEpid,
+  showEdit,
+}) {
   return (
     <table>
       <thead>
         <tr>
-        <th>Srno</th>
-          <th>Name</th>
-          <th>Emp id</th>
-          <th>Designation</th>
-          <th>Description</th>
-          <th>Date of joining</th>
-          <th>Edit</th>
+          {showSrno ? <th>Srno</th> : ""}
+          {showName ? <th>Name</th> : ""}
+          {showEpid ? <th>Emp id</th> : ""}
+          {showDesignation ? <th>Designation</th> : ""}
+          {showDescription ? <th>Description</th> : ""}
+          {showDoj ? <th>Date of joining</th> : ""}
+          {showEdit ? <th>Edit</th> : ""}
         </tr>
       </thead>
       <tbody>
         {data.map((row) => (
           <tr key={row.id}>
-            <td>{row.sno}</td>
-            <td>{row.name}</td>
-            <td>{row.id}</td>
-            <td>{row.designation}</td>
-            <td>{row.description}</td>
-            <td>{row.doj}</td>
-            <td>
-              <button onClick={() => onEdit(row)}>Edit</button>
-            </td>
+            {showSrno ? <td>{row.sno}</td> : ""}
+            {showName ? <td>{row.name}</td> : ""}
+            {showEpid ? <td>{row.id}</td> : ""}
+            {showDesignation ? <td>{row.designation}</td> : ""}
+            {showDescription ? <td>{row.description}</td> : ""}
+            {showDoj ? <td>{row.doj}</td> : ""}
+            {showEdit ? (
+              <td>
+                <button onClick={() => onEdit(row)}>Edit</button>
+              </td>
+            ) : (
+              ""
+            )}
           </tr>
         ))}
       </tbody>
